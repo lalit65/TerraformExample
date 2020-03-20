895a31b8150de8060a248041b55ced0950af068e
lalit65 <lalitvohra04@gmail.com>
2020-03-20 07:47:57 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -21,3 +21,41 @@ EOF
       tag-key = "tag-value"
   }
 }
+
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.test_role.name}"
+}
+
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "Ec2:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_instance" "role-test" {
+  ami = "ami-0bbe6b35405ecebdb"
+  instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+  key_name = "mytestpubkey"
+}
+
+##terraform init
+###terraform plan
+##terraform apply
